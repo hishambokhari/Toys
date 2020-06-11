8c57b466889326f1967d1cd0e5cbb3a7cef9a279
hishambokhari <hish.b92@gmail.com>
2020-06-11 16:16:51 +0500

    Add: Descending order for new cards created

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -3,4 +3,5 @@ class Toy < ApplicationRecord
     validates :name, presence: true, length: { minimum: 4, maximum: 100}
     validates :description, presence: true, length: { minimum: 10, maximum: 300}
 
+    default_scope -> { order(updated_at: :desc)}
 end
